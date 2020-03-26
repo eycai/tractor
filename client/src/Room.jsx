f7cc45b3bf30c568de2c30c3c12484bd77b2447d
Alex Chen <achen19@mit.edu>
2020-03-25 22:08:19 -0700

    added routing

--- client/src/Room.jsx
+++ client/src/Room.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { get } from "./api/fetch";
 
 let Room = props => {
+  // RoomID
+  useEffect(() => {
+    get("/room_info", { RoomID: props.roomid }).then(res => {
+      console.log(res);
+    });
+  }, []);
+
   return (
     <div className="Room-container">
       <div className="u-big-text">Test Room</div>
